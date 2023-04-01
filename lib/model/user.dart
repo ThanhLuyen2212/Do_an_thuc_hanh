6fbcea586229899e7c30baaf1c4df54b82512a84
ThanhLuyen2212 <nguyenhothanhluyen@gmail.com>
2023-04-01 07:48:43 +0700

    first commit

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,6 +1,22 @@
-class User {
-  String username;
-  String password;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-  User({required this.username, required this.password});
+class myUser {
+  String name;
+  String email;
+  String uid;
+
+  myUser({required this.name, required this.email, required this.uid});
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'email': email,
+        'uid': uid,
+      };
+
+  // firebase - app (user)
+  static myUser fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+    return myUser(
+        name: snapshot['name'], email: snapshot['email'], uid: snapshot['uid']);
+  }
 }
